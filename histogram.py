3a33e062640ea1744091d8c6518acc6cf77f7034
hyounce <hilde.younce@gmail.com>
2024-06-14 16:00:18 -0400

    edited histogram.py

--- histogram.py
+++ histogram.py
@@ -1,4 +1,15 @@
-x = 7
-import numpy
-import pandas
-import matplotlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_excel('MSDS.xlsx')
+values = data['CPU Number of Cores (int)'].values
+
+
+plt.hist(values, bins=12, color='thistle', edgecolor='black')
+plt.xlim(0,20)
+plt.xlabel('Number of CPU Cores')
+plt.ylabel('Number of Students')
+plt.title('CPU Cores for MSDS Students')
+plt.show()
+
